a9972f3d5d78ab665b62ee19d8e81015570390f7
Ernesto0G <ernesto7gutierrez@gmail.com>
2023-04-30 11:37:45 -0700

    Added option selection for each question

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,70 @@
 import { useEffect, useState } from 'react'
 import StartGame from './StartGame'
 import QuestionCard from './QuestionCard'
+import { nanoid } from 'nanoid'
 import './App.css'
 
 function App() {
-  const [amountOfQuestions, setAmountOfQuestions] = useState();
+  const [amountOfQuestions, setAmountOfQuestions] = useState(0);
   const [showStartGame, setShowStartGame] = useState(true);
-  const [inputValue, setInputValue] = useState(0);
   const [questions, setQuestions] = useState([]);
   const [questionCards, setQuestionCards] = useState([]);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
 
   const url = `https://opentdb.com/api.php?amount=${amountOfQuestions}&type=multiple`
-
-
-  useEffect(() => {
-    setAmountOfQuestions(inputValue);
-  }, [inputValue])
 
   useEffect(() => {
     setQuestionCards(questions.map(question => (
       <QuestionCard
-        key={question.question}
+        id={question.id}
+        key={question.id}
         question={question.question}
-        correctAnswer={question.correct_answer}
-        options={[...question.incorrect_answers, question.correct_answer]}
+        correctAnswer={question.correctAnswer}
+        options={question.options}
+        selectedOptionIndex={question.selectedOption}
+        handleOptionSelection={handleOptionSelection}
       />
     )))
+    console.log(questions)
   }, [showStartGame, questions])
 
 
   function handleStartGame() {
-    if (inputValue > 0) {
+    if (amountOfQuestions > 0) {
       fetch(url)
         .then(response => response.json())
         .then(data => {
-          setQuestions(data.results);
+          const results = data.results;
+          const questionsArray = results.map(item => ({
+            id: nanoid(),
+            question: item.question,
+            correctAnswer: item.correct_answer,
+            options: [...item.incorrect_answers, item.correct_answer],
+            selectedOptionIndex: null,
+            isOptionSelected: false
+          }))
+          setQuestions(questionsArray)
         });
       setShowStartGame(false);
     }
   }
 
-  function handleOptionSelection() {
-
+  function handleOptionSelection(id, index) {
+    setQuestions(prevState => {
+      return prevState.map(question => {
+        return question.id === id ? { ...question, selectedOptionIndex: index, isOptionSelected: true } : question
+      })
+    })
   }
 
   function handleInputChange(event) {
-    setInputValue(event.target.value);
+    setAmountOfQuestions(event.target.value);
 
   }
 
   return (
     <div className='App'>
-      {showStartGame && <StartGame inputChange={handleInputChange} inputValue={inputValue} onStartGame={handleStartGame} />}
+      {showStartGame && <StartGame inputChange={handleInputChange} inputValue={amountOfQuestions} onStartGame={handleStartGame} />}
 
       {!showStartGame &&
         <>
--- src/QuestionCard.jsx
+++ src/QuestionCard.jsx
@@ -1,18 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import he from 'he'
 
 const QuestionCard = (props) => {
-    const [isSelected, setIsSelected] = useState(false);
-    const [selectedChoice, setSelectedChoice] = useState();
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    function optionSelect(index) {
+        props.handleOptionSelection(props.id, index);
+        setSelectedOption(index)
+    }
+
     return (
         <div className='question-card'>
             <h2 className='question'>{he.decode(props.question)}</h2>
-            <h4>{props.correctAnswer}</h4>
+            {/* <h4>{props.correctAnswer}</h4> */}
             <div className="questions">
-                <button className={`option-button ${isSelected && 'selected-option'}`}>{he.decode(props.options[0])}</button>
-                <button className={`option-button ${isSelected && 'selected-option'}`}>{he.decode(props.options[1])}</button>
-                <button className={`option-button ${isSelected && 'selected-option'}`}>{he.decode(props.options[2])}</button>
-                <button className={`option-button ${isSelected && 'selected-option'}`}>{he.decode(props.options[3])}</button>
+                <button
+                    onClick={() => optionSelect(0)}
+                    className={`option-button ${selectedOption === 0 && 'selected-option'}`}>
+                    {he.decode(props.options[0])}
+                </button>
+                <button
+                    onClick={() => optionSelect(1)}
+                    className={`option-button ${selectedOption === 1 && 'selected-option'}`}>
+                    {he.decode(props.options[1])}
+                </button>
+                <button
+                    onClick={() => optionSelect(2)}
+                    className={`option-button ${selectedOption === 2 && 'selected-option'}`}>
+                    {he.decode(props.options[2])}
+                </button>
+                <button
+                    onClick={() => optionSelect(3)}
+                    className={`option-button ${selectedOption === 3 && 'selected-option'}`}>
+                    {he.decode(props.options[3])}
+                </button>
+
             </div>
             <hr />
         </div>
